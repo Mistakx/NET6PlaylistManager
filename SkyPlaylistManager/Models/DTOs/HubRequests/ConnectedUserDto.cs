8dcd37438fe2ac42320a5ec1776b466fb4540393
Milton <milton_fc_cassiano@hotmail.com>
2022-06-25 20:09:12 +0100

    Real time notifications for online followed users

---
+++ SkyPlaylistManager/Models/DTOs/HubRequests/ConnectedUserDto.cs
@@ -0,0 +1,10 @@
+﻿using SkyPlaylistManager.Models.Database;
+
+namespace SkyPlaylistManager.Models.DTOs.HubRequests
+{
+    public class ConnectedUserDto
+    {
+        public string sessionToken { get; set; }
+        public string hubconnectionId { get; set; }
+    }
+}
